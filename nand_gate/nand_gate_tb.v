fc48229f3adfc5a6fdc714efa81fb8587e7cc115
vishal <vishalsaran2006@gmail.com>
2026-09-03 18:28:06 +0530

    Add NAND gate verilog program

---
+++ nand_gate/nand_gate_tb.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+
+module nand_gate_tb;
+
+    reg  a;
+    reg  b;
+    wire y;
+
+    // Instantiate the Unit Under Test (UUT)
+    nand_gate uut (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        $display("Time\t a b | y");
+        $monitor("%0t\t %b %b | %b", $time, a, b, y);
+
+        a = 0; b = 0;
+        #10;
+
+        a = 0; b = 1;
+        #10;
+
+        a = 1; b = 0;
+        #10;
+
+        a = 1; b = 1;
+        #10;
+
+        $finish;
+    end
+
+endmodule
