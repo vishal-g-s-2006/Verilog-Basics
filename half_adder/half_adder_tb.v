970ccbc8845b999e68b253d9189fdb46aa67ef51
vishal <vishalsaran2006@gmail.com>
2026-09-09 14:34:48 +0530

    Add half adder verilog program

---
+++ half_adder/half_adder_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+module half_adder_tb;
+
+    reg  a;
+    reg  b;
+    wire sum;
+    wire carry;
+
+    // Instantiate the Unit Under Test (UUT)
+    half_adder uut (
+        .a(a),
+        .b(b),
+        .sum(sum),
+        .carry(carry)
+    );
+
+    initial begin
+        $display("Time\t a b | sum carry");
+        $monitor("%0t\t %b %b | %b    %b", $time, a, b, sum, carry);
+
+        a = 0; b = 0;
+        #10;
+
+        a = 0; b = 1;
+        #10;
+
+        a = 1; b = 0;
+        #10;
+
+        a = 1; b = 1;
+        #10;
+
+        $finish;
+    end
+
+endmodule
